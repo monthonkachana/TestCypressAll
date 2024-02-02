0757f9705763886617520aea21425f2a7fa7e9ee
monthonkachana <maxmonthonkachana2@gmail.com>
2024-02-02 14:40:17 +0700

    update file fixtures

--- cypress/e2e/Test/Fixture.cy.js
+++ cypress/e2e/Test/Fixture.cy.js
@@ -1,16 +1,34 @@
-describe('Mysuite ', () => {
-
-    it('Demo fixture Login', () => {
-
-        cy.visit('/')
-        cy.fixture('maxtest').then((data) => {
-            cy.visit('/login')
-            cy.get('#root > div > div.app-content.p-0 > div > div > form > div:nth-child(3) > input').type(data.username);
-            cy.get('#root > div > div.app-content.p-0 > div > div > form > div:nth-child(4) > input').type(data.password);
-            cy.get('#root > div > div.app-content.p-0 > div > div > form > button').click();
-        })
+describe("Mysuite", () => {
+  it.only("Check User JSON", () => {
+    cy.fixture("maxtest").then((users) => {
+      for (let i = 0; i < users.length; i++) {
+        const user = users[i];
+        cy.log(user);
+      }
+      expect(users, "User All member").to.deep.equal(users);
     });
-
-
-
+  });
+  it("Login Users Check login in vaildate", () => {
+    cy.fixture("maxtest").then((users) => {
+      for (let i = 0; i < users.length; i++) {
+        const user = users[i];
+        cy.visit("/login");
+        cy.get(
+          "#root > div > div.app-content.p-0 > div > div > form > div:nth-child(3) > input"
+        ).type(user.username);
+        cy.get(
+          "#root > div > div.app-content.p-0 > div > div > form > div:nth-child(4) > input"
+        ).type(user.password);
+        cy.get(
+          "#root > div > div.app-content.p-0 > div > div > form > button"
+        ).click();
+        cy.visit("/dashboard");
+        cy.get(".react-select__control").click({ force: true });
+        cy.get(".react-select__menu-list")
+          .find("div")
+          .should("have.length", 77);
+        cy.get(".react-select__menu-list").type("กรุง{enter}");
+      }
+    });
+  });
 });
