0a6f2a1089438caf954cfa6fd7b13fcc59c36e79
monthonkachana <maxmonthonkachana2@gmail.com>
2024-02-03 13:30:49 +0700

    push for git action

--- cypress.config.js
+++ cypress.config.js
@@ -3,14 +3,18 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   viewportWidth: 1860,
   viewportHeight: 990,
-  reporter: 'cypress-mochawesome-reporter',
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    reportPageTitle: "TESTMAX",
+    ignoreVideos: false,
+    debug: true,
+  },
   e2e: {
-     baseUrl: 'http://www.smart-insect.com:3005/',
-    
+    baseUrl: "http://www.smart-insect.com:3005/",
+
     setupNodeEvents(on, config) {
-     
       // implement node event listeners here
-      require('cypress-mochawesome-reporter/plugin')(on);
+      require("cypress-mochawesome-reporter/plugin")(on);
     },
   },
 });
