c8e7aba63ad60f350bc851dce8c37e61e333a27d
monthonkachana <maxmonthonkachana2@gmail.com>
2024-02-03 11:07:18 +0700

    update fixture V2

--- cypress/e2e/Test/Fixture.cy.js
+++ cypress/e2e/Test/Fixture.cy.js
@@ -1,6 +1,6 @@
 describe("Mysuite", () => {
-  it.only("Check User JSON", () => {
-    cy.fixture("maxtest").then((users) => {
+  it("Check User JSON", () => {
+    cy.fixture("preparedata").then((users) => {
       for (let i = 0; i < users.length; i++) {
         const user = users[i];
         cy.log(user);
@@ -9,7 +9,7 @@ describe("Mysuite", () => {
     });
   });
   it("Login Users Check login in vaildate", () => {
-    cy.fixture("maxtest").then((users) => {
+    cy.fixture("preparedata").then((users) => {
       for (let i = 0; i < users.length; i++) {
         const user = users[i];
         cy.visit("/login");
@@ -29,6 +29,26 @@ describe("Mysuite", () => {
           .should("have.length", 77);
         cy.get(".react-select__menu-list").type("กรุง{enter}");
       }
+    });
+  });
+  it("check login ID to Fix", () => {
+    cy.fixture("preparedata").then((users) => {
+      const firstID = users[0];
+      // const userid1 = users[1];
+      // cy.log(userid1);
+      cy.visit("/login");
+      cy.get(":nth-child(3) > .form-control")
+        .type(firstID.username)
+        .should("have.value", "max1");
+      cy.get(":nth-child(4) > .form-control")
+        .type(firstID.password)
+        .should("have.value", "123456789");
+      cy.get(".btn").click();
+      cy.wait(1000);
+      cy.get(".menu > :nth-child(3) > .menu-link").click();
+      cy.get("span.dropdown-item").click();
+      cy.wait(1000);
+      expect(firstID.username, "vaildate ID").to.equal(firstID.username);
     });
   });
 });
